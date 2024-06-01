20ede334a9bb3aa12a71274ca5d544d0506f1d23
Sarthak Parulekar <148764141+sarthakdev143@users.noreply.github.com>
2024-06-01 22:12:37 +0530

    Final Commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,55 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
+  const [isRight, setIsRight] = useState(false)
+
+  const handleRight = () => {
+    setIsRight(!isRight)
+  }
+
   return (
-    <header className='center1'>
-      <nav>
-        <ul type="none">
-          <li>
-            <NavLink id='link' to="/" activeClassName="active">
-              home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink id='link' className="highlight" to="/">
-              abhishek yadav
-            </NavLink>
-          </li>
-          <li>
-            <NavLink id='link' to="/services" activeClassName="active">
-              services
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      {/* <nav id='mob-nav'>
-        <ul id='dropdown' type="none">
-          <li>
-            <NavLink id='link' to="/" activeClassName="active">
-              home
-            </NavLink>
-          </li>
-          <hr />
-          <li>
-            <NavLink id='link' to="/services" activeClassName="active">
-              services
-            </NavLink>
-          </li>
-        </ul>
-        <ul id='nav-ul' type="none">
-          <li>
-            <i class="ri-menu-2-line"></i>
-          </li>
-          <li>
-            <NavLink id='link' className="highlight" to="/">
-              abhishek yadav
-            </NavLink>
-          </li>
-        </ul>
-      </nav> */}
-    </header>
+    <>
+      <header className='center1'>
+        <nav>
+          <ul type="none">
+            <li>
+              <NavLink id='link' to="/" activeClassName="active">
+                home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink id='link' className="highlight" to="/">
+                abhishek yadav
+              </NavLink>
+            </li>
+            <li>
+              <NavLink id='link' to="/services" activeClassName="active">
+                services
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <nav id='mob-nav'>
+          <ul id='nav-ul' type="none">
+            <li>
+              <i class="ri-menu-2-line" onClick={handleRight}></i>
+            </li>
+            <li>
+              <NavLink id='link' className="highlight" to="/">
+                abhishek yadav
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <ul id='dropright' type="none" className={isRight ? "doRight" : ''}>
+        <i className="ri-close-line" onClick={handleRight}></i>
+        <li>
+          <NavLink id='link' to="/" activeClassName="active">
+            home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink id='link' to="/services" activeClassName="active">
+            services
+          </NavLink>
+        </li>
+      </ul>
+    </>
   )
 }
 
